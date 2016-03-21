110f5f8f39c3085ee7ee79642de98aa9e42cbe51
Steve <steven.reimer@iamota.com>
2016-03-21 14:43:40 -0700

    Add .htaccess support and improve debug output

--- deploy/aws/cloudfront_rewrite.sh
+++ deploy/aws/cloudfront_rewrite.sh
@@ -32,20 +32,49 @@ AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:?'You need to configure the AWS_SE
 AWS_CLOUDFRONT_KEYS=${AWS_CLOUDFRONT_KEYS:?'You need to configure the AWS_CLOUDFRONT_KEYS environment variable!'}
 
 
-### Check that nginx.conf exists
-if [ -f "nginx.conf" ];
+### Check for nginx.conf
+echo -e "\e[1;40;32mCheck for nginx.conf..."
+if [ -s "nginx.conf" ];
 then
-   echo -e "\e[1;40;32mFile nginx.conf exists"
+    echo -e "\e[1;40;32mFile nginx.conf found"
+    echo -e
 else
-   echo -e "\e[1;40;31mFile nginx.conf does not exist" >&2
-   exit 1
+    echo -e "\e[1;40;33mWarning: File nginx.conf not found"
+    echo -e
+fi
+
+
+### Check for .htaccess
+echo -e "\e[1;40;32mCheck for .htaccess..."
+if [ -s ".htaccess" ];
+then
+    echo -e "\e[1;40;32mFile .htaccess found"
+    echo -e
+else
+    echo -e "\e[1;40;33mWarning: File .htaccess not found"
+    echo -e
 fi
 
 
 ### Perform Dynamic nginx.conf String Replacement
+echo -e "\e[1;40;32mPerform Dynamic nginx.conf String Replacement..."
 for key in $(echo ${AWS_CLOUDFRONT_KEYS} | sed "s/,/ /g")
 do
     value=${!key}
     temp=${value:?"You need to configure the ${key} environment variable specified in your AWS_CLOUDFRONT_KEYS!"}
-    sed -i "s/\${$key}/$value/" nginx.conf
+
+    ### Replace nginx.conf
+    if [ -s "nginx.conf" ];
+    then
+        echo -e "\e[1;40;32mReplacing \e[1;40;33m\${$key}\e[1;40;32m with '\e[1;40;33m${value}\e[1;40;32m' in nginx.conf..."
+        sed -i "s/\${$key}/$value/" nginx.conf
+    fi
+
+    ### Replace .htaccess
+    if [ -s ".htaccess" ];
+    then
+        echo -e "\e[1;40;32mReplacing \e[1;40;33m\${$key}\e[1;40;32m with '\e[1;40;33m${value}\e[1;40;32m' in .htaccess..."
+        sed -i "s/\${$key}/$value/" .htaccess
+    fi
+
 done
