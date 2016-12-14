564015e97760fcf4ef8a9426f07cd0260fbd6cae
Sangmin Yoo <sangmin.yoo@iamota.com>
2016-12-13 17:10:02 -0800

    trying to fix if statement

--- setup/dev-deploy.sh
+++ setup/dev-deploy.sh
@@ -10,20 +10,16 @@ elasticip=$(aws opsworks --region us-east-1 describe-instances --stack-id ${AWS_
 echo $elasticip
 #
 # Check if SSH is added to the instance
-if [[ ssh codeship@$elasticip ]]; then
-#
-### Rsync directly into the instance for automated testing
-# File transfer test
-### Some of the app name uses dash instead of underscore. Need to make this more consistent to avoid error.
-rsync -rave "ssh" --rsync-path="sudo rsync" . codeship@$elasticip:/mnt/httpd/${APP_NAME//-/_}/current/
-### Set ownership of the folder
-ssh codeship@$elasticip sudo chown -R apache:apache /mnt/httpd/${APP_NAME//-/_}/current/
-### remove timber cache from uploads folder
-ssh codeship@$elasticip sudo rm -rf /mnt/httpd/${APP_NAME//-/_}/current/wp-content/uploads/cache/timber/
-#
+if [ ssh "codeship@$elasticip" ]; then
+	### Rsync directly into the instance for automated testing
+	### Some of the app name uses dash instead of underscore. Need to make this more consistent to avoid error.
+	rsync -rave "ssh" --rsync-path="sudo rsync" . codeship@$elasticip:/mnt/httpd/${APP_NAME//-/_}/current/
+	### Set ownership of the folder
+	ssh codeship@$elasticip sudo chown -R apache:apache /mnt/httpd/${APP_NAME//-/_}/current/
+	### remove timber cache from uploads folder
+	ssh codeship@$elasticip sudo rm -rf /mnt/httpd/${APP_NAME//-/_}/current/wp-content/uploads/cache/timber/
 else
-#
-### Trigger OpsWorks Deployment
-aws opsworks --region us-east-1 create-deployment --stack-id ${AWS_STACK_ID} --app-id ${AWS_APP_ID} --command "{\"Name\": \"deploy\"}"
-# End if SSH is added to the instance
+	### Trigger OpsWorks Deployment
+	aws opsworks --region us-east-1 create-deployment --stack-id ${AWS_STACK_ID} --app-id ${AWS_APP_ID} --command "{\"Name\": \"deploy\"}"
+	# End if SSH is added to the instance
 fi
