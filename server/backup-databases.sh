d37809ab321ac9f909d6ba1985fb8e1e2d649544
Steve Reimer <steven.reimer@iamota.com>
2022-01-12 15:16:19 -0800

    Ensure environment is retained in backup

--- server/backup-databases.sh
+++ server/backup-databases.sh
@@ -2,9 +2,6 @@ TODAY=`date +"%Y-%b-%d"`
 BACKUP_DB_PATH="/tmp/db-backups"
 BACKUP_DB_S3="iamota-db-backups"
 
-ENVIRONMENT="${ENVIRONMENT:-unknown}"
-SERVER_NAME="${SERVER_NAME:-iamota-${ENVIRONMENT}}"
-
 mkdir -p ${BACKUP_DB_PATH}
 
 for d in /mnt/nginx/* ; do
@@ -15,20 +12,21 @@ for d in /mnt/nginx/* ; do
     if test -e "$d/current/.env"; then
         echo "-- Gathering DB credentials..."
 
+        BACKUP_DB_ENVIRONMENT=`crudini --get $d/current/.env "" "ENVIRONMENT"`
         BACKUP_DB_HOST=`crudini --get $d/current/.env "" "DB_HOST"`
         BACKUP_DB_USER=`crudini --get $d/current/.env "" "DB_USER"`
         BACKUP_DB_PASSWORD=`crudini --get $d/current/.env "" "DB_PASSWORD"`
         BACKUP_DB_NAME=`crudini --get $d/current/.env "" "DB_NAME"`
 
-        if [[ ${BACKUP_DB_HOST} && ${BACKUP_DB_USER} && ${BACKUP_DB_PASSWORD} && ${BACKUP_DB_NAME} ]]; then
+        if [[ ${BACKUP_DB_HOST} && ${BACKUP_DB_USER} && ${BACKUP_DB_PASSWORD} && ${BACKUP_DB_NAME} && ${BACKUP_DB_ENVIRONMENT} ]]; then
             echo "-- Backing up ${BACKUP_DB_NAME} from ${BACKUP_DB_USER}:****@${BACKUP_DB_HOST}..."
             
-            echo "---- Dumping DB (${BACKUP_DB_PATH}/${BACKUP_DB_NAME}-${TODAY}.sql.gz)..."
-            mysqldump --user=${BACKUP_DB_USER} --password=${BACKUP_DB_PASSWORD} --lock-tables --databases ${BACKUP_DB_NAME} | gzip > ${BACKUP_DB_PATH}/${BACKUP_DB_NAME}-${TODAY}.sql.gz
-            echo "---- Pushing to S3 (s3://${BACKUP_DB_S3}/${SERVER_NAME}/${BACKUP_DB_NAME}/${BACKUP_DB_NAME}-${TODAY}.sql.gz)..."
-            aws s3 mv ${BACKUP_DB_PATH}/${BACKUP_DB_NAME}-${TODAY}.sql.gz s3://${BACKUP_DB_S3}/${SERVER_NAME}/${BACKUP_DB_NAME}/${BACKUP_DB_NAME}-${TODAY}.sql.gz
+            echo "---- Dumping DB (${BACKUP_DB_PATH}/${BACKUP_DB_NAME}-${TODAY}--${BACKUP_DB_ENVIRONMENT}.sql.gz)..."
+            mysqldump --user=${BACKUP_DB_USER} --password=${BACKUP_DB_PASSWORD} --lock-tables --databases ${BACKUP_DB_NAME} | gzip > ${BACKUP_DB_PATH}/${BACKUP_DB_NAME}-${TODAY}--${BACKUP_DB_ENVIRONMENT}.sql.gz
+            echo "---- Pushing to S3 (s3://${BACKUP_DB_S3}/iamota-${BACKUP_DB_ENVIRONMENT}/${BACKUP_DB_NAME}/${BACKUP_DB_NAME}-${TODAY}--${BACKUP_DB_ENVIRONMENT}.sql.gz)..."
+            aws s3 mv ${BACKUP_DB_PATH}/${BACKUP_DB_NAME}-${TODAY}--${BACKUP_DB_ENVIRONMENT}.sql.gz s3://${BACKUP_DB_S3}/iamota-${BACKUP_DB_ENVIRONMENT}/${BACKUP_DB_NAME}/${BACKUP_DB_NAME}-${TODAY}--${BACKUP_DB_ENVIRONMENT}.sql.gz
             echo "---- Removing local copy..."
-            rm -rf ${BACKUP_DB_PATH}/${BACKUP_DB_NAME}-${TODAY}.sql.gz
+            rm -rf ${BACKUP_DB_PATH}/${BACKUP_DB_NAME}-${TODAY}--${BACKUP_DB_ENVIRONMENT}.sql.gz
             
             echo "-- Backup complete."
         else
